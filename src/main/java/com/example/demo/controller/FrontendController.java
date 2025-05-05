9d6f23b3dfda3e4914d3f68efc25d804ee0037c1
dispronesson <ilya.karhev@mail.ru>
2025-05-05 11:32:53 +0300

    cleared code

--- src/main/java/com/example/demo/controller/FrontendController.java
+++ src/main/java/com/example/demo/controller/FrontendController.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FrontendController {
-    @RequestMapping(value = "/{path:[^.]*}", method = RequestMethod.GET)
+    @GetMapping(value = "/{path:[^.]*}")
     public String redirect(@PathVariable String path) {
         return "forward:/index.html";
     }
